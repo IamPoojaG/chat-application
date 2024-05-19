04162d988c1b86cf52409107725c819983de5ed0
IamChetang <96056914+IamPoojaG@users.noreply.github.com>
2024-05-19 22:39:30 +0530

    checkPassword api done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import registerUser from '../controller/registerUser.js';
 import checkEmail from '../controller/checkEmail.js';
+import checkPassword from '../controller/checkPassword.js';
 
 const router = express.Router();
 
@@ -10,4 +11,6 @@ router.post('/register', registerUser);
 //check user email
 router.post('/email', checkEmail);
 
+//check user password
+router.post('/password', checkPassword);
 export default router;
