49e26a2e75592d6dd873667b2e2ad957d72cba24
IamChetang <96056914+IamPoojaG@users.noreply.github.com>
2024-05-19 21:42:07 +0530

    check email api done

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,13 @@
 import express from 'express';
 import registerUser from '../controller/registerUser.js';
+import checkEmail from '../controller/checkEmail.js';
 
 const router = express.Router();
 
+//create user api
 router.post('/register', registerUser);
+
+//check user email
+router.post('/email', checkEmail);
 
 export default router;
