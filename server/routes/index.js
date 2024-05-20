87eae64e911a1949b7f66d3918f1873b9eaaf9b1
IamChetang <96056914+IamPoojaG@users.noreply.github.com>
2024-05-20 11:58:39 +0530

    update user details and search api done

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,6 +4,8 @@ import checkEmail from '../controller/checkEmail.js';
 import checkPassword from '../controller/checkPassword.js';
 import userDetails from '../controller/userDetails.js';
 import logout from '../controller/logout.js';
+import updateUserDetails from '../controller/updateUserDetails.js';
+import searchUser from '../controller/searchUser.js';
 
 const router = express.Router();
 
@@ -21,5 +23,11 @@ router.get('/user-details', userDetails);
 
 //logout user
 router.get('/logout', logout);
+
+//update user details
+router.post('/update-user', updateUserDetails);
+
+//search  user
+router.get('/search-user', searchUser);
 
 export default router;
